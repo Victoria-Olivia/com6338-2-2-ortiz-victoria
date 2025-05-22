e2fa8cf0fb16a895fcfd9a23061590c77e773163
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-21 22:41:39 -0400

    modified to calculate and display birth year based on user input

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ let name = prompt("What is your name?");
 alert("Hello, " + name);
 
 let ageInput = prompt("How old are you?");
+
 var age = parseInt(ageInput);
 
 var hadBirthday = confirm("Did you have a birthday this year?");
 
+var currentYear = new Date().getFullYear();
+
+var birthyear;
+if (hadBirthday) {
+    birthyear = currentYear - age;
+} else {
+    birthyear = currentYear - age - 1;
+}
+
+alert("You were born in " + birthyear + ".");
 
 }
