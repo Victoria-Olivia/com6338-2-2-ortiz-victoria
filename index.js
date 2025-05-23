342601df90f63b69d4e4953297fa60711e58adee
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-23 13:59:23 -0400

    trying to clean up code for clarity

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 function greet() {
+  var name = prompt("What is your name?");
 
-var name = prompt("What is your name?");
+  alert("Hello, " + name);
 
-alert("Hello, " + name);
+  var ageInput = prompt("How old are you?");
+  var age = parseInt(ageInput);
 
-var ageInput = prompt("How old are you?");
-
-var age = parseInt(ageInput);
-
-var hadBirthday = confirm("Did you have a birthday this year?");
-
-var currentYear = new Date().getFullYear();
-
-var birthyear;
-
-if (hadBirthday) {
-    birthyear = currentYear - age;
-} else {
-    birthyear = currentYear - age - 1;
-}
-
-alert("You were born in " + birthyear + ".");
+  var hadBirthday = confirm("Did you have a birthday this year?");
+  
+  var currentYear = new Date().getFullYear();
+  var birthyear;
+  if (hadBirthday) {
+      birthyear = currentYear - age;
+  } else {
+      birthyear = currentYear - age - 1;
+  }
+  alert("You were born in " + birthyear + ".");
 
 }
