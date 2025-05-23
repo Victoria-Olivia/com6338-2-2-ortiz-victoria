4351dfcf345cededb7d60089930853db0310ebbc
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-23 10:32:04 -0400

    confused and changed 'let' to 'var' for name and ageInput declarations

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function greet() {
 
-let name = prompt("What is your name?");
+var name = prompt("What is your name?");
 
 alert("Hello, " + name);
 
-let ageInput = prompt("How old are you?");
+var ageInput = prompt("How old are you?");
 
 var age = parseInt(ageInput);
 
