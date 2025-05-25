abeb40955020b5981d29c67a780ecd8a481ecdc5
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-25 14:09:20 -0400

    Edited birthday confirmation prompt for better user clarity using ok and cancel buttons for yes and no

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function greet() {
   var ageInput = prompt("How old are you?");
   var age = parseInt(ageInput, 10);
 
-  var hasHadBirthday = confirm("Did you have a birthday this year?");
+  var hasHadBirthday = confirm("Have you have a birthday this year yet? Click OK for yes, Cancel for no.");
   
   var currentYear = new Date().getFullYear();
   var birthyear;
